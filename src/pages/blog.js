6d81db7414b39a0696d640a643dbe0b08cfd107e
Nima155 <nimashadab@hotmail.co.uk>
2021-08-13 16:15:45 +0430

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,34 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
-
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 const BlogPage = ({ data }) => {
+	console.log(data)
 	return (
 		<Layout pageTitle="My Blog Posts">
-			<ul>
-				{data.allFile.nodes.map((e) => (
-					<li key={e.id}>{e.name}</li>
-				))}
-			</ul>
+			{data.allMdx.edges.map(({ node }) => (
+				<article key={node.id}>
+					<h2>{node.frontmatter.title}</h2>{' '}
+					<p>Posted: {node.frontmatter.date}</p>
+					<MDXRenderer>{node.body}</MDXRenderer>
+				</article>
+			))}
 		</Layout>
 	)
 }
 
 export const query = graphql`
 	query {
-		allFile {
-			nodes {
-				name
-				id
+		allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+			edges {
+				node {
+					frontmatter {
+						title
+						date(formatString: "MMMM D, YYYY")
+					}
+					id
+					body
+				}
 			}
 		}
 	}
@@ -29,7 +38,7 @@ export const query = graphql`
 export default BlogPage
 
 /* gatsby has it's own graphQL data layer
-    o => Source plugins pull data from their original location into the Gatsby GraphQL data layer.
+    o=> Source plugins pull data from their original location into the Gatsby GraphQL data layer.
     o=>You can use the GraphiQL endpoint (localhost:8000/___graphql) to explore the data in the data layer and design GraphQL queries.
     o=>You can write GraphQL queries to pull data out of the data layer and into your React components.
         o=>To pull data into a “building block” component, use the useStaticQuery hook.
